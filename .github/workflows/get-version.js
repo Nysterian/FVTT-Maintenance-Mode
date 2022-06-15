582f0e694d58c891231163915d626ed6400c77ee
Nyss <30325333+Nysterian@users.noreply.github.com>
2022-06-15 15:41:55 +0100

    Setup version automation

--- .github/workflows/get-version.js
+++ .github/workflows/get-version.js
@@ -1,8 +1,14 @@
 let fs = require('fs');
 
-let moduleJSON = JSON.parse(fs.readFileSync('./module.json', 'utf8'))
-moduleJSON.name = "MaintenanceMode"
-moduleJSON.title = "Maintenance Mode"
-fs.writeFileSync('./module.json', JSON.stringify(moduleJSON), 'utf8')
+let moduleJSON = JSON.parse(fs.readFileSync('./module.json', 'utf8'));
+
+moduleJSON.name = "MaintenanceMode";
+moduleJSON.title = "Maintenance Mode";
+
+let dlLink = moduleJSON.download;
+dlLink = dlLink.substring(0, dlLink.indexOf("download/"));
+dlLink = dlLink + "download/" + moduleJSON.version + "/module.zip";
+
+fs.writeFileSync('./module.json', JSON.stringify(moduleJSON), 'utf8');
 
 console.log(moduleJSON.version);
