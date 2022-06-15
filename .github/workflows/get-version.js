2487cddb81166c263d1231f26cbedb0f2cd54e76
Nyss <30325333+Nysterian@users.noreply.github.com>
2022-06-15 15:14:34 +0100

    stringify JSON

--- .github/workflows/get-version.js
+++ .github/workflows/get-version.js
@@ -3,6 +3,6 @@ let fs = require('fs');
 let moduleJSON = JSON.parse(fs.readFileSync('./module.json', 'utf8'))
 moduleJSON.name = "MaintenanceMode"
 moduleJSON.title = "Maintenance Mode"
-fs.writeFileSync('./module.json', moduleJSON, 'utf8')
+fs.writeFileSync('./module.json', JSON.stringify(moduleJSON), 'utf8')
 
 console.log(moduleJSON.version);
