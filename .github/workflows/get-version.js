59a869d8cfab9519095dd084eb5386ca47781dd1
Nyss <30325333+Nysterian@users.noreply.github.com>
2022-06-15 18:50:46 +0100

    Dev Prep

--- .github/workflows/get-version.js
+++ .github/workflows/get-version.js
@@ -2,14 +2,11 @@ let fs = require('fs');
 
 let moduleJSON = JSON.parse(fs.readFileSync('./module.json', 'utf8'));
 
-moduleJSON.name = "MaintenanceMode";
-moduleJSON.title = "Maintenance Mode";
-
 let dlLink = moduleJSON.download;
 dlLink = dlLink.substring(0, dlLink.indexOf("download/"));
 dlLink = dlLink + "download/" + moduleJSON.version + "/module.zip";
 moduleJSON.download = dlLink;
-//--
+
 fs.writeFileSync('./module.json', JSON.stringify(moduleJSON), 'utf8');
 
 console.log(moduleJSON.version);
--- scripts/init.js
+++ scripts/init.js
@@ -1,1 +1,28 @@
-console.log("Maintenance Mode Module Initiated.")
+class maintMode {
+    static ID = 'MaintenanceMode';
+    static NAME = 'Maintenance Mode';
+
+    static FLAGS = {
+        MAINT: 'maint'
+    };
+
+    static TEMPLATES = {
+        MAINT: `modules/${this.ID}/templates/todo-list.hbs`
+    };
+
+    static log(force, ...args) {
+        const shouldLog = force || game.modules.get('_dev-mode')?.api?.getPackageDebugValue(this.ID);
+
+        if (shouldLog) {
+            console.log('%c'+this.NAME, '|', ...args);
+        }
+    }
+}
+
+Hooks.once('devModeReady', ({ registerPackageDebugFlag }) => {
+    registerPackageDebugFlag(maintMode.ID);
+});
+
+console.log(`${maintMode.NAME} | Initialised`);
+
+//maintMode.log(false, maintMode.TEMPLATES.MAINT)
